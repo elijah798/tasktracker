1f80ec446d3be31a827bae253e484bb87c1ee8b9
Elijah Sellers <46266593+elijah798@users.noreply.github.com>
2023-05-30 20:22:59 -0500

    fixing environment Variables

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,7 +5,7 @@ export const authOptions = {
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET
+            clientSecret: process.env.NEXTAUTH_SECRET
         })
     ],
 
